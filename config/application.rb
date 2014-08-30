99a1b4d6bc85c1d40bbdd010841e72cfc0b1fb11
ikepon <tatsuyanoheya@gmail.com>
2014-08-30 14:55:55 +0900

    setting not to make assets and helper file

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module Isetan
   class Application < Rails::Application
     config.generators do |g|
+      g.assets         false
+      g.helper         false
       g.test_framework :rspec,
         fixtures:         true,
         view_specs:       false,
