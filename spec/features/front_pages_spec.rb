feea9fbd6bbeacf8e529e57c24e7a50245aeee60
ikepon <tatsuyanoheya@gmail.com>
2014-09-02 23:46:03 +0900

    fix top-page-base

--- spec/features/front_pages_spec.rb
+++ spec/features/front_pages_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
-feature "トップページ", js: true do
+feature "トップページ" do
   context "ログイン前トップページの表示" do
+    background do
+      visit root_path
+    end
+
     scenario "アプリタイトルが表示されいている" do
       expect(page).to have_css('.navbar-brand', text: 'Isetan')
     end
 
     scenario "navbarのメニューが表示されている" do
-      expect(page).to have_css('ul.navbar li', text: 'Hot')
-      expect(page).to have_css('ul.navbar li', text: 'New')
-      expect(page).to have_css('ul.navbar li', text: '予約')
-      expect(page).to have_css('ul.navbar li', text: 'お問合せ')
-      expect(page).to have_css('ul.navbar li', text: 'Login')
+      expect(page).to have_css('ul.nav.navbar-nav li', text: 'Hot')
+      expect(page).to have_css('ul.nav.navbar-nav li', text: 'New')
+      expect(page).to have_css('ul.nav.navbar-nav li', text: '予約')
+      expect(page).to have_css('ul.nav.navbar-nav li', text: 'お問合せ')
+      expect(page).to have_css('ul.nav.navbar-nav li', text: 'Login')
     end
 
     scenario "メイン領域に必要コンテンツが表示されている" do
@@ -20,14 +24,14 @@ feature "トップページ", js: true do
       expect(page).to have_css('.col-md-4', text: 'Hot No2')
       expect(page).to have_css('.col-md-4', text: 'Hot No3')
 
-      expect(page).to have_css('h2', text: 'new Books')
+      expect(page).to have_css('h2', text: 'New Books')
       expect(page).to have_css('.col-md-4', text: 'New No1')
       expect(page).to have_css('.col-md-4', text: 'New No2')
       expect(page).to have_css('.col-md-4', text: 'New No3')
     end
 
     scenario "サイドバー領域に必要コンテンツが表示されている" do
-      expect(page).to have_css('.h3', text: 'News')
+      expect(page).to have_css('h3', text: 'News')
     end
 
     scenario "フッター領域に必要コンテンツが表示されている" do
