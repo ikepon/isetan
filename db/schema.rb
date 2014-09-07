bae8499365163070e0c4a18210da13e833fa384b
ikepon <tatsuyanoheya@gmail.com>
2014-09-07 11:46:03 +0900

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140907023156) do
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.text     "profile"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
