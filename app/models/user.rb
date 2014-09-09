c0dea3dbaf1e0bd432e444149a167443bb2d8a33
ikepon <tatsuyanoheya@gmail.com>
2014-09-09 23:43:44 +0900

    add validation of presence in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+
+  with_options presence: true do |required|
+    required.validates :name
+    required.validates :email
+    required.validates :password_digest
+  end
+
   mount_uploader :image, ImageUploader
 end
