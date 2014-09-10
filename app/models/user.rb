5668d295557bc4a692b86e3299f85789cd6e3ede
ikepon <tatsuyanoheya@gmail.com>
2014-09-10 23:11:54 +0900

    add the validations of lenght and unique #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
 
   with_options presence: true do |required|
-    required.validates :name
-    required.validates :email
+    required.validates :name, length: { maximum: 50 }
+    required.validates :email, uniqueness: true
     required.validates :password_digest
   end
 
