e2fa2eaf0c097c5d13e0b4b556a6304ecfd478fc
ikepon <tatsuyanoheya@gmail.com>
2014-09-13 22:29:05 +0900

    fix users factory #24

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ FactoryGirl.define do
   factory :user do
     sequence(:name) {|n| "user_name#{n}"}
     sequence(:email) {|n| "user#{n}@example.com"}
-    password_digest "user_password"
+    password "user_password"
+    password_confirmation "user_password"
     profile "my profile"
   end
 end
