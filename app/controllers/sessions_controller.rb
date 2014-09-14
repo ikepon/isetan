13c0d8efbc94132ad546c657d1eb163a33b88acf
ikepon <tatsuyanoheya@gmail.com>
2014-09-14 11:18:40 +0900

    create login #24

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'メールアドレス、パスワードの組み合わせに誤りがあります。'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
