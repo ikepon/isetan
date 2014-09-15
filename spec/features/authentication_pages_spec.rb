43cb3a57d502bffb022ce02ecba5ee2ce104018a
ikepon <tatsuyanoheya@gmail.com>
2014-09-15 12:21:53 +0900

    fix login-out spec #24

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -5,42 +5,51 @@ feature 'ログイン、ログアウト' do
     let!(:user) { create(:user) }
 
     before do
-      visit new_user_path
+      visit login_path
     end
 
     scenario '正しい情報を入れるとログインできる' do
-      fill_in 'user[email]', with: 'user1@example.com'
-      fill_in 'user[password]', with: 'user_password'
-      click_on 'ログインする'
+      fill_in 'session[email]', with: 'user1@example.com'
+      fill_in 'session[password]', with: 'user_password'
+
+      within '.login' do
+        click_on 'Login'
+      end
 
       expect(page).to have_css('.user-name', text: 'user_name1')
+      expect(page).to have_link('Profile', href: user_path(user))
+      expect(page).to have_link('Logout', href: logout_path)
+      expect(page).not_to have_link('Login', href: login_path)
+
     end
 
     scenario '間違ったemailを入れるとログインできない' do
-      fill_in 'user[email]', with: 'other@example.com'
-      fill_in 'user[password]', with: 'user_password'
+      fill_in 'session[email]', with: 'other@example.com'
+      fill_in 'session[password]', with: 'user_password'
 
-      click_on 'ログインする'
+      within '.login' do
+        click_on 'Login'
+      end
 
-      expect(page).to have_css('.alert', text: '入力情報に誤りがあります。')
+      expect(page).to have_css('.alert', text: 'メールアドレス、パスワードの組み合わせに誤りがあります。')
     end
 
     scenario '間違ったpasswordを入れるとログインできない' do
-      fill_in 'user[email]', with: 'user1@example.com'
-      fill_in 'user[password]', with: 'other_password'
+      fill_in 'session[email]', with: 'user1@example.com'
+      fill_in 'session[password]', with: 'other_password'
 
-      click_on 'ログインする'
+      within '.login' do
+        click_on 'Login'
+      end
 
-      expect(page).to have_css('.alert', text: '入力情報に誤りがあります。')
+      expect(page).to have_css('.alert', text: 'メールアドレス、パスワードの組み合わせに誤りがあります。')
     end
 
     context 'ログアウトする' do
-      scenario 'ログインした状態で、ログアウトする' do
-        include_context 'ユーザーとしてログインしている'
+      include_context 'ユーザーとしてログインしている'
 
-        within '.nav' do
-          click_on 'logout'
-        end
+      scenario 'ログインした状態で、ログアウトする' do
+        find('.dropdown-menu a', text: 'Logout').click
 
         expect(page).to have_css('h2', text: '蔵書管理 isetan')
       end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,12 +1,15 @@
-shared_context 'ユーザーとしてログインしている' do |user|
-  let!(:current_user) { create(user) }
+shared_context 'ユーザーとしてログインしている' do
+  let!(:current_user) { create(:user) }
 
   background do
     visit login_path
 
-    fill_in 'email', with: current_user.email
-    fill_in 'password_digest', with: current_user.password_digest
+    fill_in 'session[email]', with: current_user.email
+    fill_in 'session[password]', with: current_user.password
 
-    click_on 'ログインする'
+    within '.login' do
+      click_on 'Login'
+    end
   end
 end
+
