b02904a7f20b010499397a1ace836de6d0ad8d56
ikepon <tatsuyanoheya@gmail.com>
2014-09-15 11:29:26 +0900

    add login function #24

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
+      flash[:success] = "Welcome to isetan"
       redirect_to user
     else
       flash.now[:danger] = 'メールアドレス、パスワードの組み合わせに誤りがあります。'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   with_options presence: true do |required|
     required.validates :name, length: { maximum: 50 }
@@ -10,4 +11,18 @@ class User < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   has_secure_password
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
