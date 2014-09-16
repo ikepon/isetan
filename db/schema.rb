f97adb21a85d8a0e3285a5930d1de293fd2815ea
ikepon <tatsuyanoheya@gmail.com>
2014-09-17 00:45:11 +0900

    create news model #10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915093048) do
+ActiveRecord::Schema.define(version: 20140916150949) do
+
+  create_table "news", force: true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name",                           null: false
