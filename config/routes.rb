2427166783fdc1deb1200c4a42713dff05ac4b0b
ikepon <tatsuyanoheya@gmail.com>
2014-09-17 00:43:14 +0900

    create news routes #10

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'front_pages/index'
 
   resources :users, only: %i(index show new create edit update)
+  resources :news, only: %i(index show new create)
 
   resources :sessions, only: %i(new create destroy)
   match '/signup', to: 'users#new',        via: 'get'
