d7ec8aa0b87b731ff93d42f6dfb3f32ec30e37aa
ikepon <tatsuyanoheya@gmail.com>
2014-09-30 23:48:27 +0900

    fix books factory #8

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :book do
     sequence(:title) {|n| "BookTitle#{n}"}
-    sequence(:isbn) {|n| "123456789{n}"}
-    wanted 1
-    read 1
-    rental 1
+    sequence(:isbn) {|n| "123456789#{n}"}
+    sequence(:wanted) {|n| "1#{n}"}
+    sequence(:read) {|n| "3#{n}"}
+    sequence(:rental) {|n| "5#{n}"}
   end
 end
