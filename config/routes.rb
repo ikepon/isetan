74362044566ab591a44045deb7bc9ef8a6bd18ab
ikepon <tatsuyanoheya@gmail.com>
2014-12-26 08:53:41 +0900

    Add reviews_controller base #15

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i(index show new create edit update)
   resources :news, only: %i(index show new create)
-
   resources :collections
+  resources :reviews
 
   resources :sessions, only: %i(new create destroy)
   match '/signup', to: 'users#new',        via: 'get'
