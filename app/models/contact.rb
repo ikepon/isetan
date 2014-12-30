a22b47773e87dc7494ca83c13fe4c3ce48b0479c
ikepon <tatsuyanoheya@gmail.com>
2014-12-30 19:10:31 +0900

    Add validation for contact model #44

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  validates :category, presence: true
+  validates :name,     presence: true, length: { maximum: 255 }
+  validates :email,    presence: true, length: { maximum: 255 }
+  validates :content,  presence: true
 end
