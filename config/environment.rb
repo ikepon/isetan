9b455a3ee896c446292c8f3b20023d5900522438
ikepon <tatsuyanoheya@gmail.com>
2015-01-02 23:09:36 +0900

    Add amazon search setting #40

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Amazon::Ecs.options = {
+  :associate_tag =>     '987654321',
+  :AWS_access_key_id => 'amazonaccesskey',
+  :AWS_secret_key =>   'amazonsecretkey'
+}
