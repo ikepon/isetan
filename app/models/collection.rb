42164b81a95f36ea76f5825ecd25d04e229038e2
ikepon <tatsuyanoheya@gmail.com>
2015-01-09 08:55:14 +0900

    Use 'enum' for Collection status #23

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,7 @@
 class Collection < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
+
+  # ステータス: reservable: 予約可, rented: 貸出中, expired: 返却遅延
+  enum status: {reservable: 1, rented: 2, expired: 3}
 end
