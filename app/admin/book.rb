b84f741a6f3fba2b6d2345075c52df05992f329b
ikepon <tatsuyanoheya@gmail.com>
2015-01-10 23:05:17 +0900

    Can change book_cover for activeadmin #57

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Book do
-  permit_params :title, :asin, :wanted, :read, :rental
+  permit_params :title, :asin, :wanted, :read, :rental, :book_cover
+
+  form html: { enctype: 'multipart/form-data' } do |f|
+    f.inputs 'Book' do
+      f.input :title
+      f.input :asin
+      f.input :wanted
+      f.input :read
+      f.input :rental
+      f.input :book_cover, as: :file
+    end
+    f.actions
+  end
+
 end
