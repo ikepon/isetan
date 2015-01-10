a6511fd6646d7dc0138fce72739eaec08406be29
ikepon <tatsuyanoheya@gmail.com>
2015-01-10 23:06:02 +0900

    Can change user image for activeadmin #36

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
-  permit_params :name, :email, :password_digest, :profile, :image, :remember_token, :current_sign_in_at, :role
+  permit_params :name, :email, :password, :profile, :image, :remember_token, :current_sign_in_at, :role
+
+  form html: { enctype: 'multipart/form-data' } do |f|
+    f.inputs 'User' do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :profile
+      f.input :image, as: :file
+      f.input :role
+    end
+    f.actions
+  end
 end
