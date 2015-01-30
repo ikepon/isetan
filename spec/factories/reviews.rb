69a84294758d743ca7a1ef259e10eceb7d3283e2
ikepon <tatsuyanoheya@gmail.com>
2015-01-31 01:18:35 +0900

    Fix factories #19

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
   factory :review do
-    user_id 1
-    book_id 1
+    user nil
+    book nil
     title "この本は名著ですね！"
     content "いや〜、すばらしい！すばらしすぎて言葉がでない。"
     evaluation 5
+
+    trait :review_whatever do
+      user { create(:user) }
+      book { create(:book) }
+    end
   end
 end
