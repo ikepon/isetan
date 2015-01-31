33e234cb4b06dcc971a07fcbb81ad4cb6c418f1a
ikepon <tatsuyanoheya@gmail.com>
2015-01-31 09:29:18 +0900

    Fix reveiws#show spec #19

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature '読書感想ページ', js: true do
+feature '読書感想', js: true do
   context '感想一覧ページ' do
     include_context 'ユーザーとしてログインしている'
 
@@ -45,6 +45,40 @@ feature '読書感想ページ', js: true do
 
         expect(find_field('Title').value).to eq '最高の本'
       end
+    end
+  end
+
+  context '感想詳細ページ' do
+    include_context 'ユーザーとしてログインしている'
+
+    let!(:review) { create(:review, :review_whatever, title: '最高の本') }
+
+    background do
+      visit reviews_path
+    end
+
+    scenario '詳細ページに遷移して、必要項目が表示されている' do
+      expect(page).to have_css('h2', text: '読書感想一覧')
+
+      click_link '最高の本'
+
+      expect(page).to have_css('h2', text: '最高の本')
+      expect(page).to have_css('h3', text: review.book.title)
+      expect(page).to have_content(review.user.name)
+      expect(page).to have_content(review.content)
+    end
+
+    scenario 'ログインしているユーザが書いた感想の編集ボタンは本人のみ表示される' do
+      create(:review, :review_whatever, title: 'ログインユーザの感想', user_id: current_user.id)
+
+      click_link '最高の本'
+
+      expect(page).not_to have_css('a.edit-btn', text: '編集')
+
+      click_link '読書感想'
+      click_link 'ログインユーザの感想'
+
+      expect(page).to have_css('a.edit-btn', text: '編集')
     end
   end
 end
