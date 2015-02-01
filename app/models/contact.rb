66591e19f3c803e7332f05a887b433777a1cc3f4
ikepon <tatsuyanoheya@gmail.com>
2015-02-01 16:16:58 +0900

    Add contacts#new test #19

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
   validates :category, presence: true
-  validates :name,     presence: true, length: { maximum: 255 }
+  validates :name,     presence: true, length: { maximum: 50 }
   validates :email,    presence: true, length: { maximum: 255 }
   validates :content,  presence: true
 end
---
+++ spec/features/contacts_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+feature 'お問い合わせページ', js: true do
+  scenario 'TOPページからお問い合わせページに遷移できる' do
+    visit root_path
+
+    within '.navbar-collapse' do
+      click_link 'お問合せ'
+    end
+
+    expect(page).to have_css('h2', text: 'お問合せ')
+  end
+
+  context 'お問合せをする' do
+    background do
+      visit new_contact_path
+    end
+
+    scenario 'お問合せできる' do
+      expect(page).to have_css('h2', text: 'お問合せ')
+
+      choose '機能について'
+      fill_in 'Name', with: 'カミナ'
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'Content', with: 'もっとおもしろい機能を追加してほしい'
+
+      click_on '確認する'
+
+      expect(page).to have_css('h2', text: 'お問合せ内容の確認')
+      expect(page).to have_content('カミナ')
+      expect(page).to have_content('test@example.com')
+      expect(page).to have_content('もっとおもしろい機能を追加してほしい')
+
+      click_on '送信する'
+
+      expect(page).to have_css('h2', text: 'お問合せ完了')
+      expect(page).to have_css('.alert.alert-success', text: 'お問合せが完了しました')
+
+      click_link 'TOPページに戻る'
+
+      expect(page).to have_css('.navbar-brand', text: 'Isetan')
+    end
+
+    scenario '空白のまま確認ボタンを押すとエラーがでる' do
+      click_on '確認する'
+
+      expect(page).to have_css('h2', text: 'お問合せ')
+
+      within '.alert.alert-danger' do
+        expect(page).to have_content("Name can't be blank")
+        expect(page).to have_content("Email can't be blank")
+        expect(page).to have_content("Content can't be blank")
+      end
+    end
+
+    scenario '名前が50文字、メールアドレスが255文字より多い場合はエラーがでる' do
+      fill_in 'Name', with: 'a' * 56
+      fill_in 'Email', with: 'a' * 256
+
+      click_on '確認する'
+
+      expect(page).to have_css('h2', text: 'お問合せ')
+
+      within '.alert.alert-danger' do
+        expect(page).to have_content("Name is too long (maximum is 50 characters)")
+        expect(page).to have_content("Email is too long (maximum is 255 characters)")
+      end
+
+    end
+  end
+end
