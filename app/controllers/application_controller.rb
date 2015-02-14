11eb5814bc31222ecb3e1535f65569b23e2101cb
ikepon <tatsuyanoheya@gmail.com>
2015-02-14 15:30:47 +0900

    Add the latest 3 contents of reviews and collections in sidebar #66

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
 
   def sidebar
     @sidebar_news = News.order(created_at: :desc).limit(3)
+    @sidebar_reviews = Review.eager_load(:user).order(created_at: :desc).limit(3)
+    @sidebar_collections = Collection.eager_load(:book, :user).order(created_at: :desc).limit(3)
   end
 end
