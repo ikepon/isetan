c08e710f14294314ea53903648bd93ee247d3b17
ikepon <tatsuyanoheya@gmail.com>
2015-02-18 08:45:31 +0900

    Add mypage home #71

--- app/controllers/mypage/profile_controller.rb
+++ app/controllers/mypage/profile_controller.rb
@@ -1,9 +1,32 @@
 class Mypage::ProfileController < ApplicationController
+  before_action :signed_in_user, only: %w(edit update)
+
   layout 'mypage'
 
   def edit
+    @user = current_user
   end
 
   def update
+    # TODO パスワード入力なしでも更新できるように
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールを更新しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :image, :image_cache, :sign_in_count, :current_sign_in_at)
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to login_url, notice: "ログインしてください"
+    end
   end
 end
