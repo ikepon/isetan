ad42dd80c53eb7fa0e261b0d5482d6770958a1c2
ikepon <tatsuyanoheya@gmail.com>
2015-02-22 12:20:19 +0900

    Add company account #72

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      if user.role?
-        can :manage, :all
-      else
-        # 管理者以外
-      end
+    if user.role == 1
+      can :manage, :all
+    elsif user.role == 2
+      # 会社アカウントで本の貸出を管理
+    else
+      # 管理者以外
     end
+  end
 end
