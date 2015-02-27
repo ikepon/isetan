572ba99992ce4d0a160cc00d09017221e0e342cd
ikepon <tatsuyanoheya@gmail.com>
2015-02-27 09:22:42 +0900

    Add lends view #72

--- app/controllers/lends_controller.rb
+++ app/controllers/lends_controller.rb
@@ -3,10 +3,12 @@ class LendsController < ApplicationController
 
   def index
     company_account = User.where(role: 2)
-    @company_collections = Collection.where(user: company_account).page(params[:page]).per(7)
+    @company_collections = Collection.where(user: company_account).order(created_at: :desc).page(params[:page]).per(7)
   end
 
   def show
+    @collection = Collection.find(params[:id])
+    @book = @collection.book
   end
 
   def edit
