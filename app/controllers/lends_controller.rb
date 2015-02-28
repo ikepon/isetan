e514e3348ee36f70321256a4c2a72f2d7d6012e4
ikepon <tatsuyanoheya@gmail.com>
2015-02-28 12:43:43 +0900

    Delete unnecessary action

--- app/controllers/lends_controller.rb
+++ app/controllers/lends_controller.rb
@@ -1,6 +1,4 @@
 class LendsController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
-
   def index
     company_account = User.where(role: 2)
     @company_collections = Collection.where(user: company_account).order(created_at: :desc).page(params[:page]).per(7)
@@ -9,11 +7,5 @@ class LendsController < ApplicationController
   def show
     @collection = Collection.find(params[:id])
     @book = @collection.book
-  end
-
-  def edit
-  end
-
-  def update
   end
 end
