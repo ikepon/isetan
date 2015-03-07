e7c6f69e55107325cf1eb6ce24466ad145d11ce9
ikepon <tatsuyanoheya@gmail.com>
2015-03-07 17:24:39 +0900

    Fix layouts for mypage lends

--- app/controllers/mypage/lends_controller.rb
+++ app/controllers/mypage/lends_controller.rb
@@ -1,4 +1,8 @@
 class Mypage::LendsController < ApplicationController
+  before_action :signed_in_user
+
+  layout 'mypage'
+
   def index
     @rentals = Collection.rental_books.where(borrower: current_user, status: [1, 2]).page(params[:page]).per(7)
   end
